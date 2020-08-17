777b50ce8748c488f35d1bf4d2810c4173d880db
carninojo15 <carlosmasson96@gmail.com>
2020-08-17 14:47:55 -0400

    test 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import BayList from './components/BayList';
+//import CurrentBay from './components/CurrentBay';
 import './App.css';
+import 'antd/dist/antd.css';
+import {Layout} from 'antd';
+const{Sider, Header , Footer, Content} = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+function App(){
+  const [data, setData]= useState([]);
+  useEffect(() => {
+    const fetchData = async() =>{
+      const result = await fetch("data.json")
+      .then( response => response.json());
+      setData(result);
+    };
+    fetchData();
+  },[]);
+
+  return(
+    
+    //<CurrentBay/>
+    <Layout className = "layout"  >
+      <Header className ="header" style = {{backgroundColor:"green"}}>NavBar</Header>
+      <Layout >
+        
+        <Sider width ="300" className ="sider"> <BayList bays={data}/></Sider>
+        <Content className ="content" style = {{backgroundColor:"purple"}}>Hello WOrld</Content>
+      </Layout>
+      <Footer style = {{backgroundColor:"green"}}><h1>extra info</h1></Footer>
+    </Layout>
   );
 }
 
